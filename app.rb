3a29c41405836dc36782983303fe82281cfa4ea8
NoFoster <kfoster0@chicagobooth.edu>
2025-04-14 21:24:46 +0000

    Calculate Square

--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/square/new") do
+
+  erb(:new_square_calc)
+end
+
 get("/howdy") do
 
   erb(:howdy)
 
 end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num =params.fetch("users_number").to_f
+  
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
 
 get("/") do
   "
