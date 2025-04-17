4cb66fd9da7275db062d641a6d4cb4b673f4a454
NoFoster <kfoster0@chicagobooth.edu>
2025-04-17 21:20:32 +0000

    APR_Calculator

--- app.rb
+++ app.rb
@@ -34,9 +34,36 @@ get("/square_root/results") do
   erb(:square_rt_results)
 end 
 
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @user_min = params.fetch("user_min").to_f
+  @user_max = params.fetch("user_max").to_f
+
+  @the_result= rand(@user_min..@user_max)
+erb(:random_result)
+end
+
+get ("/payment/new") do
+  erb(:new_apr_calc)
+end
+
+get("/payment/results") do
+
+  @user_apr = params.fetch("user_apr").to_f
+  @user_years = params.fetch("user_years").to_f
+  @user_principal = params.fetch("user_principal").to_f
+
+  @numerator = (@user_apr / (100*12) ) * @user_principal
+  @denominator = 1 - (1 + @user_apr) ** (-@user_years * 12)
+  @the_result = @numerator / @denominator
+
+  erb(:apr_results)
+end
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:homepage)
 end
